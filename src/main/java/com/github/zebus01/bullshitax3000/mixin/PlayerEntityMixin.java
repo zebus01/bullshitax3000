c2b0e33317cc7c4b24313ffcc7d1b057b40fd9c1
zebus01 <maellllll01@gmail.com>
2023-06-06 12:50:01 +0200

    add method knockBackHand at attack head in PlayerEntityMixin

--- src/main/java/com/github/zebus01/bullshitax3000/mixin/PlayerEntityMixin.java
+++ src/main/java/com/github/zebus01/bullshitax3000/mixin/PlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package com.github.zebus01.bullshitax3000.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
@@ -7,10 +8,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
@@ -26,6 +29,16 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             System.out.println("client ? " + this.world.isClient());
             BlockPos.iterateRandomly(this.random, (stack.getCount() / 4) + 1, this.getBlockPos(), stack.getCount()/4).forEach((BlockPos blockPos) ->
                     this.world.createExplosion(null, blockPos.getX(), this.getY(), blockPos.getZ(), stack.getCount() / 8.0f, World.ExplosionSourceType.TNT));
+        }
+    }
+
+    @Inject(method = "attack", at = @At("HEAD"))
+    private void knockBackWand(Entity target, CallbackInfo ci)
+    {
+        if (!world.isClient()) {
+            if (this.getMainHandStack().isOf(Items.STICK)) {
+                ((LivingEntity)target).takeKnockback(this.getMainHandStack().getCount(), MathHelper.sin(this.getYaw() * ((float)Math.PI / 180)), -MathHelper.cos(this.getYaw() * ((float)Math.PI / 180)));
+            }
         }
     }
 }
